1a126ec55c0446dad6907ad4db3afc2c2b6903f4
ywy50 <wysocki.yannick@gmail.com>
2024-07-10 17:34:09 +0800

    sqlmap script

--- scripts/sqlmap.sh
+++ scripts/sqlmap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Target in format "http://192.168.1.202/?p=1&forumaction=search"
 export TARGET=$1
-sqlmap -u $TARGET --dbs
+shift # Shift positional parameters to the left (i.e., $2 becomes $1, $3 becomes $2, etc.)
+sqlmap -u $TARGET --dbs "$@"
