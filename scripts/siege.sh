725eb0232b1698d824da70fac162a5f02c649377
ywy50 <wysocki.yannick@gmail.com>
2024-07-10 17:02:14 +0800

    update scripts

--- scripts/siege.sh
+++ scripts/siege.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Target will be DNS name or IP address, e.g. https://google.com
-TARGET=$1
+export TARGET=$1
 siege --verbose --time=2M --concurrent=100 $TARGET > siege.results
--- scripts/thc-ssl-dos.sh
+++ scripts/thc-ssl-dos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Target is usually an IP address value, e.g. 192.168.1.208
-TARGET=$1
+export TARGET=$1
 thc-ssl-dos -l $TARGET 443 --accept
