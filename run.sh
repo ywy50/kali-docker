7d1c38e00e8a555243693440462883d08f85398d
ywy50 <wysocki.yannick@gmail.com>
2024-07-10 18:13:39 +0800

    force prune

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 sudo docker build -t kali-tools:latest .
-sudo docker image prune 
+sudo docker image prune -f
 sudo docker run -it --rm -v $(pwd)/volume:/opt/app/volume kali-tools:latest /bin/bash
