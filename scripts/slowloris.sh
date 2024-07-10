e680eaf4884f18cae4db56a893945094662eee9a
ywy50 <wysocki.yannick@gmail.com>
2024-07-10 17:08:13 +0800

    slowloris

--- scripts/slowloris.sh
+++ scripts/slowloris.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Target in format http://192.168.1.202/index.php
 export TARGET=$1
-slowhttptest -c 1000 -H -g -o slowhttp -i 10 -r 200 -t GET -u $TARGET -x 24 -p 3
+slowhttptest -c 1000 -H -g -o slowhttp -i 30 -r 200 -t GET -u $TARGET -x 24 -p 3
