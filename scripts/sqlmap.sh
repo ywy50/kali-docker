b39543444e92fae90cfb600c44dd32964a33f4a4
ywy50 <wysocki.yannick@gmail.com>
2024-07-10 18:48:38 +0800

    sqlmap comment

--- scripts/sqlmap.sh
+++ scripts/sqlmap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Target in format "http://192.168.1.202/?p=1&forumaction=search"
+#target specific dbms, add as parameter when runnning script, e.g. --dbms=PostgreSQL
 export TARGET=$1
 shift # Shift positional parameters to the left (i.e., $2 becomes $1, $3 becomes $2, etc.)
 sqlmap -u $TARGET --dbs "$@"
