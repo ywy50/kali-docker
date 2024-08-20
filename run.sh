bb1047e8eb6cf2422b5547302636cba1f20e2ac0
ywy50 <wysocki.yannick@gmail.com>
2024-08-20 14:50:36 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run -it --rm --name kali -v $(pwd)/volume:/opt/app/volume ywy50/kali-tools:v0.0.5 /bin/bash
+sudo docker run -it --rm --name kali -v $(pwd)/volume:/opt/app/volume ywy50/kali-tools:v0.0.5
+# /bin/bash
